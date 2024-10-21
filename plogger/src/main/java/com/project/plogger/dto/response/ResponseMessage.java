974f834c1e454e3fe6db34a5e236829986dbc856
mikieoo <jcw2551@naver.com>
2024-10-21 12:52:52 +0900

    first commit

--- plogger/src/main/java/com/project/plogger/dto/response/ResponseMessage.java
+++ plogger/src/main/java/com/project/plogger/dto/response/ResponseMessage.java
@@ -10,12 +10,9 @@ public interface ResponseMessage {
     String NO_EXIST_USER_ID = "No exist user id.";
 
     String TEL_AUTH_FAIL = "Tel number authentication failed.";
-<<<<<<< HEAD
     String NO_PERMISSION = "No permission.";
-=======
     String SIGN_IN_FAIL = "Sign in failed.";
     String AUTHENTICATION_FAIL = "ㅇㄹㅇㄹㅇ.";
->>>>>>> 7570330adf36d1be952764a62e530432916e6887
 
     String MESSAGE_SEND_FAIL = "Auth number send failed.";
     String TOKEN_CREATE_FAIL = "Token creation failed.";
