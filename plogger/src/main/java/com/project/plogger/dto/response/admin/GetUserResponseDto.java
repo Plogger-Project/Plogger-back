4452ef15c678ea6df68ac0f9de6a3a6bb06ba356
mikieoo <jcw2551@naver.com>
2024-10-21 12:45:56 +0900

    first commit

--- plogger/src/main/java/com/project/plogger/dto/response/admin/GetUserResponseDto.java
+++ plogger/src/main/java/com/project/plogger/dto/response/admin/GetUserResponseDto.java
@@ -3,7 +3,6 @@ package com.project.plogger.dto.response.admin;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.project.plogger.common.object.User;
 import com.project.plogger.dto.response.ResponseCode;
 import com.project.plogger.dto.response.ResponseDto;
 import com.project.plogger.dto.response.ResponseMessage;
--- plogger/src/main/java/com/project/plogger/filter/JwtAuthenticationFilter.java
+++ plogger/src/main/java/com/project/plogger/filter/JwtAuthenticationFilter.java
@@ -1,14 +1,10 @@
 package com.project.plogger.filter;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
