0406868b07ec30c259a10f362433f9c5de5172d7
Jeongsunga <jsa3341@gmail.com>
2024-10-21 14:49:17 +0900

    gifticon back finish

--- plogger/src/main/java/com/project/plogger/controller/GifticonController.java
+++ plogger/src/main/java/com/project/plogger/controller/GifticonController.java
@@ -1,15 +1,20 @@
 package com.project.plogger.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.plogger.dto.request.gifticon.PatchGifticonRequestDto;
 import com.project.plogger.dto.request.gifticon.PostGifticonRequestDto;
 import com.project.plogger.dto.response.ResponseDto;
 import com.project.plogger.dto.response.gifticon.GetGifticonListResponseDto;
+import com.project.plogger.dto.response.gifticon.GetGifticonResponseDto;
 import com.project.plogger.service.GifticonService;
 
 import jakarta.validation.Valid;
@@ -35,5 +40,30 @@ public class GifticonController {
         ResponseEntity<? super GetGifticonListResponseDto> response = gifticonService.getGifticonList();
         return response;
     }
+
+    @GetMapping("/{gifticonId}")
+    public ResponseEntity<? super GetGifticonResponseDto> getGifticon(
+        @PathVariable("gifticonId") Integer gifticonId
+    ){
+        ResponseEntity<? super GetGifticonResponseDto> response = gifticonService.getGifticon(gifticonId);
+        return response;
+    };
+
+    @PatchMapping("/{gifticonId}")
+    public ResponseEntity<ResponseDto> patchGifticon(
+        @PathVariable("gifticonId") Integer gifticonId,
+        @RequestBody @Valid PatchGifticonRequestDto requestBody
+    ){
+        ResponseEntity<ResponseDto> response = gifticonService.patchGifticon(gifticonId, requestBody);
+        return response;
+    };
+
+    @DeleteMapping("/{gifticonId}")
+    public ResponseEntity<ResponseDto> deleteGifticon(
+        @PathVariable("gifticonId") Integer gifticonId
+    ){
+        ResponseEntity<ResponseDto> response = gifticonService.deleteGifticon(gifticonId);
+        return response;
+    };
     
 }
--- plogger/src/main/java/com/project/plogger/service/GifticonService.java
+++ plogger/src/main/java/com/project/plogger/service/GifticonService.java
@@ -2,6 +2,7 @@ package com.project.plogger.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.project.plogger.dto.request.gifticon.PatchGifticonRequestDto;
 import com.project.plogger.dto.request.gifticon.PostGifticonRequestDto;
 import com.project.plogger.dto.response.ResponseDto;
 import com.project.plogger.dto.response.gifticon.GetGifticonListResponseDto;
@@ -11,6 +12,7 @@ public interface GifticonService {
 
     ResponseEntity<ResponseDto> postGifticon(PostGifticonRequestDto dto);
     ResponseEntity<? super GetGifticonListResponseDto> getGifticonList();
-    // ResponseEntity<? super GetGifticonResponseDto> getGifticon(Integer gifticonId);
-    // ResponseEntity<ResponseDto> patchGifticon(Integer gifticonId, PatchGifticonRequestDto dto);   
+    ResponseEntity<? super GetGifticonResponseDto> getGifticon(Integer gifticonId);
+    ResponseEntity<ResponseDto> patchGifticon(Integer gifticonId, PatchGifticonRequestDto dto);  
+    ResponseEntity<ResponseDto> deleteGifticon(Integer gifticonId); 
 }
