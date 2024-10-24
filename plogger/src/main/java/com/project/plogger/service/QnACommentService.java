27ddfd092583d38c93204b7d6bbaebf756bc34fd
kijung <kijung991008@naver.com>
2024-10-24 11:31:08 +0900

    SNS 로그인

--- plogger/src/main/java/com/project/plogger/service/QnACommentService.java
+++ plogger/src/main/java/com/project/plogger/service/QnACommentService.java
@@ -10,6 +10,6 @@ public interface QnACommentService {
 
     ResponseEntity<ResponseDto> postQnAComment(PostQnACommentRequestDto dto, String userId, Integer qnaId);
     ResponseEntity<? super GetQnACommentListResponseDto> getQnACommentList(Integer qnaPostId);
-    // ResponseEntity<ResponseDto> patchQnAComment(Integer qnaId, PatchToolRequestDto dto);
+    // ResponseEntity<ResponseDto> patchTool(Integer qnaId, PatchToolRequestDto dto);
     
 }
