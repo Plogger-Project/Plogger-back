c12b4250e7b7775378806b18f96b8f534250e8e9
imwkdrh <imwkdrh7@naver.com>
2024-10-25 17:30:49 +0900

    2024.10.25 17:30

--- plogger/src/main/java/com/project/plogger/config/WebSecurityConfig.java
+++ plogger/src/main/java/com/project/plogger/config/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig {
                                 .sessionManagement(sessionManagement -> sessionManagement
                                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                                 .authorizeHttpRequests(request -> request
-                                                .requestMatchers("/", "/api/v1/auth/**", "/file/*","/find-id/*", "/send-auth/*","/api/v1/recruit").permitAll()
+                                                .requestMatchers("/", "/api/v1/auth/**", "/file/*","/find-id/*", "/send-auth/*","/api/v1/recruit","/api/v1/qna","/api/v1/active").permitAll()
                                                 .requestMatchers("/api/v1/user/**").hasRole("USER")
                                                 .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                                                 .anyRequest().authenticated())
