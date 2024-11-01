7ded3d5d5f1d6182c46345e5ec2a802061075172
daeun <laihan3518@gmail.com>
2024-11-01 12:17:43 +0900

    활동 신고글 조회

--- plogger/src/main/java/com/project/plogger/service/ActiveReportService.java
+++ plogger/src/main/java/com/project/plogger/service/ActiveReportService.java
@@ -4,8 +4,11 @@ import org.springframework.http.ResponseEntity;
 
 import com.project.plogger.dto.request.active.ActiveReportRequestDto;
 import com.project.plogger.dto.response.ResponseDto;
+import com.project.plogger.dto.response.active.GetActiveReportListResponseDto;
 
 public interface ActiveReportService {
     
     ResponseEntity<ResponseDto> createReport(Integer activeId, String userId, ActiveReportRequestDto dto);
+
+    ResponseEntity<? super GetActiveReportListResponseDto> getAllActiveReportPost();
 }
