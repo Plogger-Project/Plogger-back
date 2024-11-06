1f1c0556bea01a91dda5ae8782d6c3664356c49f
Jeongsunga <jsa3341@gmail.com>
2024-11-06 09:14:36 +0900

    get recruit scrap

--- plogger/src/main/java/com/project/plogger/dto/request/recruit/PatchRecruitIsCompletedRequestDto.java
+++ plogger/src/main/java/com/project/plogger/dto/request/recruit/PatchRecruitIsCompletedRequestDto.java
@@ -1,6 +1,5 @@
 package com.project.plogger.dto.request.recruit;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
